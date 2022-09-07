fb0f736e4b66bd7314ccc4ffe175c2d2d0a6117f
Jamal4hm3d <Jamal24633@gmail.com>
2022-09-07 13:23:00 +0530

    file ext add

--- src/lib/components/Poster.svelte
+++ src/lib/components/Poster.svelte
@@ -1,10 +1,15 @@
 <script>
+  import is from "is-it-check";
   export let details;
   const params = new URLSearchParams({ title: details.title }).toString();
+  let fileName = `${details.name}.m3u8`;
+  if (is.mobile()) {
+    fileName = `${details.name}.xspf`;
+  }
 </script>
 
 <div class="flex-container">
-  <a href={`/api/rd?${params}`} download={`${details.name}.m3u8`}>
+  <a href={`/api/rd?${params}`} download={fileName}>
     <img src={details.image} alt={details.name} /></a
   >
   <div class="name">{details.name}</div>
